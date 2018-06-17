4e82201aad3f72d3c14d1236529d71806972212f
ZhIgOl <igor.zhurov.work@gmail.com>
2018-06-17 14:51:33 +0300

    20170617
    03

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+/*
+$(function(){
+
+	$('a[data-target^="anchor"]').bind('click.smoothscroll', function(){
+		var target = $(this).attr('href'),
+			bl_top = $(target).offset().top - 50;
+		$('body, html').animate({scrollTop: bl_top}, 500);
+		return false;
+	});
+
+
+});
+*/
+
+
+
+
+
